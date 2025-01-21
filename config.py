ffc6893128d82370d7f3279535618612503b5df8
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2025-01-21 18:12:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "27412915"))
 
 API_HASH = getenv("API_HASH", "3fcf3b84e1bad89d67c216c0750da858")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5963562690:AAEYXKKJbGzUDGK4fLxo53IB5YCCsHhb9NU")
+BOT_TOKEN = getenv("BOT_TOKEN", "8147450157:AAHT4Qx1rQvyKeZ7geYmXrGGc9f8LTqonXg")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hny:zara@cluster0.lfe5o.mongodb.net/?retryWrites=true&w=majority")
 
@@ -27,7 +27,7 @@ BOTADDLOGS = int(getenv("BOTADDLOGS", "-1001603822916"))
 
 OWNER_ID = int(getenv("OWNER_ID", "7552579717"))
 
-BOT_USERNAME = getenv("BOT_USERNAME" , "AloneXMusicBot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "RedWineXMusicBot")
 
 COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
 

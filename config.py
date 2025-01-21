aa9beb35a3e564dbc9ea9a0da7b42b488d0066c9
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2025-01-21 18:13:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DilwarHosen/Spy",
+    "https://github.com/DilwarHosen/Red-Wine",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ALONE")
 GIT_TOKEN = getenv(
